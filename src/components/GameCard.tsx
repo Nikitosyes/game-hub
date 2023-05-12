8337c328e03390ba48743f7ffcec8d655ee4f0eb
Nick <nick.sieriebriakov@gmail.com>
2023-05-12 19:44:59 +0300

    added emojis

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,6 +3,7 @@ import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
+import Emoji from "./Emoji";
 
 interface Props {
   game: Game;
@@ -21,6 +22,7 @@ const GameCard = ({ game }: Props) => {
         </HStack>
         <Heading marginBottom={"10px"} fontSize="2xl">
           {game.name}
+          <Emoji rating={game.rating_top}></Emoji>
         </Heading>
       </CardBody>
     </Card>
